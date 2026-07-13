f2185c94a58339957d0f055e767de9a22609b38b
fang20030527 <zhenyufang162@gmail.com>
2026-07-13 09:57:00 +0800

    Google AdSense代码上传

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7093360885280732"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-82YS9SBYPS"
