4d99ea721adaca77bce3d51349317de54d724db8
fang20030527 <zhenyufang162@gmail.com>
2026-07-13 09:46:03 +0800

    GA4埋点

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 
 import { siteMetadata } from "./seo";
 import "./globals.css";
@@ -27,6 +28,19 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>{children}</body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-82YS9SBYPS"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-82YS9SBYPS');
+        `}
+      </Script>
     </html>
   );
 }
